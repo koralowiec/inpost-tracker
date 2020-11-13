8ddabb8bad8607a34c2a4cb6c44a68c9fd723e0c
koralowiec <arek0906@protonmail.com>
2020-11-13 09:11:50 +0100

    Getting statues and some tracking info from inpost api

---
+++ data/api.go
@@ -0,0 +1,125 @@
+package data
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+const apiUrl = "https://api-shipx-pl.easypack24.net/v1/"
+const trackingEndpoint = apiUrl + "tracking/"
+const statusesEndpoint = apiUrl + "statuses/"
+
+type Status struct {
+	Name        string `json:"name"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type Statuses []Status
+
+func (statues Statuses) findMatchingStatus(status string) Status {
+	for _, s := range statues {
+		if s.Name == status {
+			return s
+		}
+	}
+
+	return Status{}
+}
+
+type StatusesResponse struct {
+	Items Statuses `json:"items"`
+}
+
+type TrackingDetail struct {
+	Status   Status
+	DateTime time.Time `json:"datetime"`
+}
+
+func (t *TrackingDetail) UnmarshalJSON(data []byte) error {
+	var v map[string]string
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+
+	}
+
+	datetime, err := time.Parse(time.RFC3339, v["datetime"])
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.DateTime = datetime
+
+	s, err := GetStatuses()
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Status = s.findMatchingStatus(v["status"])
+
+	return nil
+}
+
+type TrackingResponse struct {
+	TrackingNumber  string           `json:"tracking_number"`
+	TrackingDetails []TrackingDetail `json:"tracking_details"`
+}
+
+var statuses Statuses
+
+func GetStatuses() (Statuses, error) {
+	if statuses != nil {
+		return statuses, nil
+	}
+
+	return fetchStatuses()
+}
+
+func fetchStatuses() ([]Status, error) {
+	res, err := http.Get(statusesEndpoint)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var resJson StatusesResponse
+	if err := json.Unmarshal(data, &resJson); err != nil {
+		return nil, err
+	}
+	statuses = resJson.Items
+
+	return statuses, nil
+}
+
+func GetTrackingInfo(trackingNumber string) (*TrackingResponse, error) {
+	if statuses != nil {
+		if _, err := GetStatuses(); err != nil {
+			return nil, err
+		}
+	}
+
+	url := trackingEndpoint + "/" + trackingNumber
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var resJson TrackingResponse
+	if err := json.Unmarshal(data, &resJson); err != nil {
+		return nil, err
+	}
+
+	return &resJson, nil
+}
--- main.go
+++ main.go
@@ -1,21 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"gitlab.com/koralowiec/inpost-track/data"
-)
-
 const filePath = "/home/arek/.inpost-track/saved.json"
 
 func main() {
-	a := data.LoadFileContent()
-	fmt.Println(a.TrackingNumbers)
-	a.AppendTrackingNumber("2137")
-	fmt.Println(a.TrackingNumbers)
-	err := a.SaveFileContent()
-	if err != nil {
-		log.Fatal(err)
-	}
 }
